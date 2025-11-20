ca1d698bd0acffa484527643f23b2c0dc95b5906
Eggmanaa <eggmanaa@example.com>
2025-11-19 21:59:30 -0800

    Fix Cloudflare 404: Add SPA routing and refactor functions

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,4 +1,4 @@
-import api from '../src/api/index';
+import api from '../../src/api/index';
 
 export const onRequest: PagesFunction = async (context) => {
   return api.fetch(context.request, context.env, context);
